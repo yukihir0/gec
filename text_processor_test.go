246c8e0e4b8fc71772e6a3f9ffeb39ff227d9fa8
yukihir0 <yukihiro.cotori@gmail.com>
2015-07-17 06:38:19 +0900

    fix text_processor test

--- text_processor_test.go
+++ text_processor_test.go
@@ -2,33 +2,13 @@ package gec
 
 import (
 	"fmt"
-	"os"
 	"testing"
 )
 
-func TestMain(m *testing.M) {
-	os.Exit(run(m))
-}
-
-func run(m *testing.M) int {
-	setup()
-	defer teardown()
-	code := m.Run()
-	return code
-}
-
-var opt *Option
-var tp *TextProcessor
-
-func setup() {
-	opt = NewOption()
-	tp = NewTextProcessor(opt)
-}
-
-func teardown() {
-}
-
 func TestZenkakuASCII2HankakuASCIILowerAlphabet(t *testing.T) {
+	opt := NewOption()
+	tp := NewTextProcessor(opt)
+
 	actual := tp.zenkaku2Hankaku("ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ")
 	expected := "abcdefghijklmnopqrstuvwxyz"
 
@@ -38,6 +18,9 @@ func TestZenkakuASCII2HankakuASCIILowerAlphabet(t *testing.T) {
 }
 
 func TestZenkakuASCII2HankakuASCIIUpperAlphabet(t *testing.T) {
+	opt := NewOption()
+	tp := NewTextProcessor(opt)
+
 	actual := tp.zenkaku2Hankaku("ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ")
 	expected := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -47,6 +30,9 @@ func TestZenkakuASCII2HankakuASCIIUpperAlphabet(t *testing.T) {
 }
 
 func TestZenkakuASCII2HankakuASCIINumber(t *testing.T) {
+	opt := NewOption()
+	tp := NewTextProcessor(opt)
+
 	actual := tp.zenkaku2Hankaku("０１２３４５６７８９")
 	expected := "0123456789"
 
@@ -56,6 +42,9 @@ func TestZenkakuASCII2HankakuASCIINumber(t *testing.T) {
 }
 
 func TestZenkakuASCII2HankakuASCIISymbol(t *testing.T) {
+	opt := NewOption()
+	tp := NewTextProcessor(opt)
+
 	actual := tp.zenkaku2Hankaku("：；＜＝＞？＠［＼］＾＿｀｛｜｝～！＂＃＄％＆＇（）＊，－．／a")
 	expected := ":;<=>?@[\\]^_`{|}~!\"#$%&'()*,-./a"
 
@@ -65,6 +54,9 @@ func TestZenkakuASCII2HankakuASCIISymbol(t *testing.T) {
 }
 
 func TestZenkakuSpace2HankakuSpace(t *testing.T) {
+	opt := NewOption()
+	tp := NewTextProcessor(opt)
+
 	actual := tp.zenkaku2Hankaku("　")
 	expected := " "
 
@@ -74,6 +66,9 @@ func TestZenkakuSpace2HankakuSpace(t *testing.T) {
 }
 
 func TestCharref2Ascii(t *testing.T) {
+	opt := NewOption()
+	tp := NewTextProcessor(opt)
+
 	actual := tp.charref2Ascii("&nbsp;&lt;&gt;&amp;&laquo;&raquo;")
 	expected := " <>&«»"
 
@@ -83,6 +78,9 @@ func TestCharref2Ascii(t *testing.T) {
 }
 
 func TestRemoveKeisen(t *testing.T) {
+	opt := NewOption()
+	tp := NewTextProcessor(opt)
+
 	for r := 0x2500; r <= 0x257F; r++ {
 		actual := tp.eliminateRuledLine(fmt.Sprintf("%c", r))
 		expected := ""
@@ -94,6 +92,9 @@ func TestRemoveKeisen(t *testing.T) {
 }
 
 func TestEliminateUselessTags(t *testing.T) {
+	opt := NewOption()
+	tp := NewTextProcessor(opt)
+
 	actuals := []string{
 		tp.EliminateUselessTags("<script>hoge</script><style>fuga</style><select><option>a</option></select><noscript>foo</noscript>"),
 		tp.EliminateUselessTags("<!-- comment\ncomment -->"),
@@ -119,6 +120,9 @@ func TestEliminateUselessTags(t *testing.T) {
 }
 
 func TestHasOnlyTags(t *testing.T) {
+	opt := NewOption()
+	tp := NewTextProcessor(opt)
+
 	actuals := []bool{
 		tp.IsOnlyTags("\t <br> \n"),
 		tp.IsOnlyTags("\t hoge \n"),
@@ -136,6 +140,9 @@ func TestHasOnlyTags(t *testing.T) {
 }
 
 func TestEliminateLink(t *testing.T) {
+	opt := NewOption()
+	tp := NewTextProcessor(opt)
+
 	actuals := []string{
 		tp.EliminateLink("<div>content</div><li>1.hoge</li><li>2.fuga</li></ul>"),
 		tp.EliminateLink("<div>content</div><a href=\"/hoge\">hoge</a><a href=\"/fuga\">fuga</a><ul><li>1.hoge</li><li>2.fuga</li></ul>"),
@@ -153,6 +160,9 @@ func TestEliminateLink(t *testing.T) {
 }
 
 func TestIsLinklist(t *testing.T) {
+	opt := NewOption()
+	tp := NewTextProcessor(opt)
+
 	actuals := []bool{
 		tp.isLinkList("<ul><li>1.hoge</li><li>2.fuga</li></ul>"),
 		tp.isLinkList("start 123<ul><li>1.hoge</li><li>2.fuga</li></ul>456 end"),
@@ -170,6 +180,9 @@ func TestIsLinklist(t *testing.T) {
 }
 
 func TestStripTags(t *testing.T) {
+	opt := NewOption()
+	tp := NewTextProcessor(opt)
+
 	actual := tp.EliminateTags("Ａ　ＢＣ\342\224\200<br>\n<br>&lt;&gt; \t\n ", "")
 	expected := "A BC\n<> \n"
 
@@ -177,3 +190,14 @@ func TestStripTags(t *testing.T) {
 		t.Errorf("expected %v, but got %v", expected, actual)
 	}
 }
+
+/*
+func TestEvaluatelist(t *testing.T) {
+	actual := tp.caluculateLinkRate([]string{"hoge", "<a href=\"/hoge\">hoge</a>", "<a href=\"/fuga\">fuga</a>", "<a href=\"/foo\">foo</a>"})
+	expected := 6.0625
+
+	if actual != expected {
+		t.Errorf("expected %v, but got %v", expected, actual)
+	}
+}
+*/
